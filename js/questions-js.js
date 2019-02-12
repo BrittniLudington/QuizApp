714cf25dc7affb065d8e10734cbbbca110ab638d
BrittniLudington <brittniludington@gmail.com>
2019-02-11 16:17:04 -0800

    changed question build to dynamically built and made question result

--- js/questions-js.js
+++ js/questions-js.js
@@ -7,5 +7,86 @@ export const questions =
         answer : "c",
         reason : "7 is spelled Seven. Remove the 'S' and you get even",
         image : "./img/1.png"
-    }
+    },
+    {
+        number : 2,
+        question : "The more you take, the more you leave behind. What is it?",
+        options : {a : "Shadow", b : "Fingerprints", c : "Footprints", d : "Love"},
+        answer : "b",
+        reason : "Any thing you grab or touch will have your fingerprints on them.",
+        image : "./img/2.png"
+    },
+    {
+        number : 3,
+        question : "What cannot be burned in a fire or drowned in water?",
+        options : {a : "Wood", b : "Stone", c : "Crystal", d : "Ice"},
+        answer : "d",
+        reason : "Ice does not burn, it melts. Ice floats on top of water instead of sinking.",
+        image : "./img/3.png"
+    },
+    {
+        number : 4,
+        question : "Everytime you lose something, you'll always find it in the very last place you looked, why is that?",
+        options : {a : "It was in the last place I looked.", b : "I didn't think about looking there.", c : "My luck is terrible.", d : "I don't organize any of my things."},
+        answer : "a",
+        reason : "It's in the last place you looked because once you find it, there's no reason to keep looking.",
+        image : "./img/4.png"
+    },
+    {
+        number : 5,
+        question : "What can run but never walks,"+
+        "has a mouth but never talks,"+
+        "has a bed but never sleeps,"+
+        "has a head but never weeps?",
+        options : {a : "The ocean", b : "A robot", c : "A river", d : "A doll"},
+        answer : "c",
+        reason : "All four lines relate to parts of a river.",
+        image : "./img/5.png"
+    },
+    {
+        number : 6,
+        question : "What has to be broken before it can be used?",
+        options : {a : "An egg", b : "A pair of shoes", c : "Items encased in that asinine clamshell packaging", d : "A can of soda"},
+        answer : "a",
+        reason : "To use an egg in cooking, you have to break it first to get the yolk inside.",
+        image : "./img/6.png"
+    },
+    {
+        number : 7,
+        question : "You can hold it without using your hands or arms. What is it?",
+        options : {a : "Your vision", b : "Your breath", c : "Your thought", d : "Your memories"},
+        answer : "b or c",
+        reason : "You can hold your breath, and you can hold that thought. Both are popular phrases.",
+        image : "./img/7.png"
+    },
+    {
+        number : 8,
+        question : "It's so fragile even saying its name can break it. What is it?",
+        options : {a : "Glass", b : "He who must not be named", c : "Silence", d : "A ghost"},
+        answer : "c",
+        reason : "Saying 'silence', or anything, creates noise and breaks the silence.",
+        image : "./img/8.png"
+    },
+    {
+        number : 9,
+        question : "Do you want a free point?",
+        options : {a : "Yes", b : "Absolutely", c : "Of course", d : "Why is this even here?"},
+        answer : "a b c or d",
+        reason : "All the above. Have a freebie",
+        image : "./img/9.png"
+    },
+    {
+        number : 10,
+        question : "Finale, let's shake things up."+
+        "Two women went out for drinks one night. They both ordered the same drink."+
+        "One of the women was incredibly thirsty and finished five in the time it took" +
+        "the other to finish just one. As it had turned out the drinks were poisoned,"+
+        "but only the lady who drank one drink died. How can this be?",
+        options : {a : "The surviving lady was immune", b : "The deceased lady's drink had a higher amount of poison", c : "One was allergic the other was not", d : "The surviving lady drank hers too quick for the poison"},
+        answer : "d",
+        reason : "The poison was hidden inside the drinks' ice cubes. The deceased lady"+
+        "drank slowly enough for the ice to melt and poison the rest of the drink, while the other"+
+        "finished each glass before the poison had time to thaw.",
+        image : "./img/10.png"
+    },
 ];
--- js/quiz-js.js
+++ js/quiz-js.js
@@ -6,8 +6,12 @@ $(document).ready(function()
 {
     setTitle();
     handleTitleButtonClick();
+    handleQuestionButtons();
+    handleContinueButton();
     currentQuestion = 0;
     score = new Array(10);
+    renderScore();
+    //setResult("c");
 
 });
 
@@ -16,7 +20,7 @@ function setTitle()
     // hides anything not affiliated with title page and shows anything that is.
     $(".scoreHeader-js").hide();
     $(".question-js").hide();
-
+    $(".questionResult-js").hide();
     $(".mainPage-js").show();
 
 }
@@ -26,17 +30,29 @@ function handleTitleButtonClick()
     $('.startButton-js').on('click',function(event)
     {
         event.preventDefault();
-        renderQuestionMenu();
         setQuestion();
     });
 }
 
-function renderQuestionMenu()
+function handleQuestionButtons()
 {
-    $('.answerButton-js').on('click',function(event)
+    $('.question-js').on('click','.answerButton-js',function(event)
     {
-        console.log("answer" + $(this).attr("value") + " clicked.");
-        setResult();
+        console.log("answer " + $(this).attr("value") + " clicked.");
+        setResult($(this).attr("value"));
+    });
+}
+
+function handleContinueButton()
+{
+    $('.questionResult-js').on('click','.continueButton-js',function(event)
+    {
+        console.log("continue button clicked");
+        if(currentQuestion <=9)
+        {
+            currentQuestion++;
+            setQuestion();
+        }
     });
 }
 
@@ -46,27 +62,65 @@ function setQuestion()
     $(".scoreHeader-js").show();
     $(".question-js").show();
 
+    $(".questionResult-js").hide();
     $(".mainPage-js").hide();
     renderQuestion();
+}
+
+function renderScore()
+{
+    let count = 0;
+    for(let i = 0; i < score.length; i++)
+    {
+        if(score[i] == 1)
+            count++;
+    }
+    $(".scoreHeader-js").html("<p>Score: " + count + "</p>");
 }
 
 function renderQuestion()
 {
     const curQuestion = questions[currentQuestion];
-    $("#questionNum").text("Question " +curQuestion.number);
-    $("blockquote").text(curQuestion.question);
+    let string = " <h1 id = 'questionNum'>Question "+curQuestion.number+"</h1>";
+    string += "<blockquote>"+curQuestion.question+"</blockquote>";
+    string += "<main id = 'menu'>";
+    string += "<ul id='options' class = 'options-js'>";
     for(var k in curQuestion.options)
     {
-        $("button[type='button'][value="+k+"]").text(k + ".) "+curQuestion.options[k]);
-        //console.log(k, curQuestion.options[k]);
+        string +=  "<li><button type = 'button' class = 'answerButton-js' value = "+k+">"+k+".) "+curQuestion.options[k]+"</button></li>";
     }
-    $("img").attr("src",curQuestion.image);
-  
+    string += "</ul>";
+    string += "<figure id='questionImage'><img src = "+curQuestion.image+"></figure>";
+    string += "</main>";
+    $(".question-js").html(string);
 }
 
-function setResult()
+function setResult(chosenAnswer)
 {
-    $(".scoreHeader-js").hide();
+    $(".scoreHeader-js").show();
     $(".question-js").hide();
     $(".mainPage-js").hide();
+    $(".questionResult-js").show();
+    let ans = 0;
+    if(questions[currentQuestion].answer.includes(chosenAnswer))
+    {
+        console.log("correct");
+        ans = "CORRECT";
+        score[currentQuestion] = 1;
+    }
+    else
+    {
+        console.log("incorrect");
+        ans = "INCORRECT";
+        score[currentQuestion] = 0;
+    }
+    renderScore();
+    let string = "<h1 id = 'resultHeader'>"+ans+"</h1>";
+    string += "<div id = 'reasoning'>";
+    string += "<h2>Answer: "+questions[currentQuestion].answer+"</h2>";
+    string += "<p>"+questions[currentQuestion].reason+"</p>";
+    string += "</div>";
+    string += "<button type='button' id = 'continueButton' class = 'continueButton-js'>Continue</button>";
+    $(".questionResult-js").html(string);
+
 }
